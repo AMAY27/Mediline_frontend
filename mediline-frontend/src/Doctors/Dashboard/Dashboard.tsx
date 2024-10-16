df626f4467ae1fa7c7fd62aaf04c1986a644b026
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-10-16 21:58:52 +0530

    Base vertical nav bar setup

--- mediline-frontend/src/Doctors/Dashboard/Dashboard.tsx
+++ mediline-frontend/src/Doctors/Dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import BaseNav from '../Components/BaseNav/BaseNav'
 
 const Dashboard = () => {
   return (
-    <>
-        <Navbar/>
-        <div className='font-bold text-green-300 text-xl'>
-            Doctor clinic name
+    <div className='flex'>
+        <BaseNav/>
+        <div className='w-full'>
+            <Navbar/>
+            <div className='font-bold text-green-300 text-xl'>
+                Doctor clinic name
+            </div>
         </div>
-    </>
+    </div>
   )
 }
 
