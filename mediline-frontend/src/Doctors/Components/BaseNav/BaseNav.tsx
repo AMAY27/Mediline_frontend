6a4b8e8ae2b213786cdf65c8f574c190fc6cda35
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-10-21 19:18:31 +0530

    Active navigation item indication setup

--- mediline-frontend/src/Doctors/Components/BaseNav/BaseNav.tsx
+++ mediline-frontend/src/Doctors/Components/BaseNav/BaseNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaDesktop, FaRegCalendar } from "react-icons/fa";
 import { MdManageAccounts } from "react-icons/md";
 import { IoCalendarClearOutline } from "react-icons/io5";
@@ -8,31 +8,48 @@ import { CiChat1 } from "react-icons/ci";
 
 
 const BaseNav = () => {
+    const [clickedNavItem, setClickedNavItem] = useState<String>("admin");
+    const handleNavItemsClick = (item:String) => {
+        setClickedNavItem(item);
+    }
   return (
     <div className='flex flex-col items-center bg-green-800 pt-5 w-[7%] h-screen'>
         <div className='bg-green-600 font-bold rounded-full px-2 text-[2rem] font-bold flex items-center mb-4'>
             <h2 className='text-green-200'>M</h2>
         </div>
-        <div className='flex flex-col items-center text-center text-sm break-words w-full py-2 text-white mb-4 bg-green-600'>
+        <div 
+            onClick={() => handleNavItemsClick("admin")}
+            className={`flex flex-col items-center text-center text-sm break-words w-full py-2 cursor-pointer text-white mb-4 hover:bg-green-600 ${clickedNavItem === "admin" ? 'bg-green-600' : 'bg-transparent'}`}
+        >
             <div className='text-3xl'>
                 <SlScreenDesktop/>
             </div>
             <h2>Admin</h2>
         </div>
-        <div className='flex flex-col items-center text-center text-sm break-words mb-4 text-white'>
+        <div 
+            onClick={() => handleNavItemsClick("patients")}
+            className={`flex flex-col items-center text-center text-sm break-words w-full py-2 cursor-pointer mb-4 text-white hover:bg-green-600 ${clickedNavItem === "patients" ? 'bg-green-600' : 'bg-transparent'}`}
+        >
             <div className='text-3xl'>
                 <MdManageAccounts/>
             </div>
             <h2>Patients</h2>
             {/* <h2 className='text-ellipsis overflow-hidden ...'>management</h2> */}
         </div>
-        <div className='flex flex-col items-center text-center text-sm break-words mb-4 text-white'>
+        <div 
+            onClick={() => handleNavItemsClick("calendar")}
+            className={`flex flex-col items-center text-center text-sm break-words w-full py-2 cursor-pointer mb-4 text-white hover:bg-green-600 ${clickedNavItem === "calendar" ? 'bg-green-600' : 'bg-transparent'}`}
+        
+        >
             <div className='text-3xl'>
                 <IoCalendarClearOutline/>
             </div>
             <h2>Calendar</h2>
         </div>
-        <div className='flex flex-col items-center text-center text-sm break-words mb-4 text-white'>
+        <div 
+            onClick={() => handleNavItemsClick("chat")}
+            className={`flex flex-col items-center text-center text-sm break-words w-full py-2 cursor-pointer mb-4 text-white hover:bg-green-600 ${clickedNavItem === "chat" ? 'bg-green-600' : 'bg-transparent'}`}
+        >
             <div className='text-3xl'>
                 <CiChat1/>
             </div>
