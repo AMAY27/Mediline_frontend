79ca9ac028f0f2703310492eaf6899daa3232722
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-11-04 20:39:25 +0530

    Appointment booking form components restructuring

---
+++ mediline-frontend/src/Doctors/Admin/AdminComponents/AppointmentBooking/AppointmentBookingMain.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import BookingForm from './BookingForm';
+import ContactForm from './ContactForm';
+import FormInput from './FormInput';
+import RegistrationForm from './RegistrationForm';
+
+
+const AppointmentBookingMain = () => {
+    const [contactVerified, setContactVerified] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isContactVerificationComplete, setIsContactVerificationComplete] = useState(false);
+    const [contactNumber, setContactNumber] = useState<number>();
+
+    const mockContactVerification = (contact: number) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(contact === 1234567890);
+            }, 1000);
+        });
+    };
+    const handleContactSubmit = async (contact:number) => {
+        setIsLoading(true);
+        try {
+            const contactExists = await mockContactVerification(contact);
+            if (contactExists) {
+                setIsContactVerificationComplete(true);
+                setContactVerified(true); 
+            } else {
+                setIsContactVerificationComplete(true);
+                setContactVerified(false);
+                alert("User not found, please register first.");
+            }
+            if (!contactExists) alert("User not found, please register first.");
+        } catch {
+            alert("Error connecting to mock verification.");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    return (
+        <div>
+            {!isContactVerificationComplete ? (
+                <ContactForm 
+                    contactNumber={contactNumber} 
+                    setContactNumber={setContactNumber} 
+                    handleContactSubmit={handleContactSubmit} 
+                    isLoading={isLoading} 
+                />
+            ) : contactVerified ? (
+                <BookingForm />
+            ) : (
+                <RegistrationForm />
+            )}
+        </div>
+    );
+}
+
+export default AppointmentBookingMain
---
+++ mediline-frontend/src/Doctors/Admin/AdminComponents/AppointmentBooking/BookingForm.tsx
@@ -0,0 +1,27 @@
+// BookingForm.tsx
+import React from 'react';
+import FormInput from './FormInput';
+
+const BookingForm: React.FC = () => {
+    return (
+        <form className='space-y-4'>
+            <h2 className='text-xl'>Appointment Booking</h2>
+            <FormInput label="First Name" placeholder="Enter First Name" required />
+            <FormInput label="Last Name" placeholder="Enter Last Name" required />
+            <div className="grid grid-cols-2 gap-4">
+                <FormInput label="Age" placeholder="Enter Age" required />
+                <FormInput label="Weight" placeholder="Kg" />
+                <FormInput label="Contact" placeholder="Enter Contact" />
+                <FormInput label="Blood Pressure" bloodPressure />
+            </div>
+            <button 
+                className='p-2 flex justify-center font-bold rounded-md w-full border-2 border-green-600 hover:bg-green-600 hover:text-white text-green-600'
+                type='submit'
+            >
+                Complete Booking
+            </button>
+        </form>
+    );
+};
+
+export default BookingForm;
